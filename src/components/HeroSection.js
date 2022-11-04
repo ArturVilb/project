5d85414f3711c74416d6c5167dfec5911751abce
Artur Vilbik <arapaima999@mail.ru>
2022-11-04 20:49:33 +0200

    project5commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -13,7 +13,7 @@ function HeroSection() {
         <div className="hero-btns">
             <button className='btns' buttonStyle='btn--primary' buttonSize='btn--large' onClick={() => setButtonPopup(true)}>WATCH OFFICIAL VIDEO<i className='far fa-play-circle'/></button>
             <Video trigger={buttonPopup} setTrigger={setButtonPopup}>
-                  <video autoPlay controls src='../../assets/Archetype- Tim Henson.mp4'></video>
+                  <video autoPlay controls src='../../assets/Polyphia - Goose (Official Music Video).mp4'></video>
             </Video>
         </div>
     </div>
