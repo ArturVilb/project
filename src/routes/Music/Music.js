a78f5987337b4c1dd9e666dc830af4d23fb760a2
Artur Vilbik <arapaima999@mail.ru>
2022-11-15 20:53:25 +0200

    projectmusicplayercommit

--- src/routes/Music/Music.js
+++ src/routes/Music/Music.js
@@ -2,12 +2,57 @@ import React from 'react';
 import '../../App.css';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/footer/Footer';
+import Player from '../../components/player/Player';
+import {useState, useEffect} from 'react';
+
+import IMG1 from '../../interactive assets/goose.jpg'
+import IMG2 from '../../interactive assets/40oz.jpg'
+
+import MUSIC1 from '../../interactive assets/music/Polyphia_Goose.mp3'
+import MUSIC2 from '../../interactive assets/music/Polyphia_40oz.mp3'
 
 export default function Music() {
+
+const [songs] = useState([
+    {
+        title: "Goose",
+        artist: "Poliphya",
+        img_src: IMG1,
+        src: MUSIC1
+    },
+    {
+        title: "40oz",
+        artist: "Poliphya",
+        img_src: IMG2,
+        src: MUSIC2
+    }
+    ])
+
+
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+    useEffect(() => {
+        setNextSongIndex(() => {
+            if(currentSongIndex + 1 > songs.length - 1) {
+                return 0;
+            } else {
+                return currentSongIndex + 1;
+            }
+        });
+    }, [currentSongIndex])
+
     return (
         <>
             <Navbar />
-            <h1 className="music">MUSIC</h1>
+            <div className='Music'>
+                <Player 
+                currentSongIndex={currentSongIndex}
+                setCurrentSongIndex={setCurrentSongIndex}
+                nextSongIndex={nextSongIndex}
+                songs={songs}
+                />
+            </div>
             <Footer />
         </>
       )
